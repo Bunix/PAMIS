5a30472e69b9d489a9f1818cb9559ea8a9a4aca7
Bunix <etechnohubshop@gmail.com>
2017-01-26 14:20:30 +0800

    files source

---
+++ application/controllers/Personnel.php
@@ -0,0 +1,348 @@
+<?php
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+/* 	
+ * 	@author : Bunix
+ * 	16th January, 2017
+ 
+ */
+
+class Personnel extends CI_Controller
+{
+    
+    
+    function __construct()
+    {
+        parent::__construct();
+		$this->load->database();
+        $this->load->library('session');
+        /*cache control*/
+        $this->output->set_header('Last-Modified: ' . gmdate("D, d M Y H:i:s") . ' GMT');
+        $this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+        $this->output->set_header('Pragma: no-cache');
+        $this->output->set_header("");
+    }
+    
+    /***default functin, redirects to login page if no admin logged in yet***/
+    public function index()
+    {
+        if ($this->session->userdata('personnel_login') != 1)
+            redirect(base_url() . 'index.php?login', 'refresh');
+        if ($this->session->userdata('personnel_login') == 1)
+            redirect(base_url() . 'index.php?personnel/dashboard', 'refresh');
+    }
+    
+    /***ADMIN DASHBOARD***/
+    function dashboard()
+    {
+        if ($this->session->userdata('personnel_login') != 1)
+            redirect(base_url(), 'refresh');
+        $page_data['page_name']  = 'dashboard';
+        $page_data['page_title'] = get_phrase('personnel_dashboard');
+        $this->load->view('backend/index', $page_data);
+    }
+    
+    
+    /****MANAGE TEACHERS*****/
+    function teacher_list($param1 = '', $param2 = '', $param3 = '')
+    {
+        if ($this->session->userdata('personnel_login') != 1)
+            redirect(base_url(), 'refresh');
+        if ($param1 == 'personal_profile') {
+            $page_data['personal_profile']   = true;
+            $page_data['current_teacher_id'] = $param2;
+        }
+        $page_data['teachers']   = $this->db->get('teacher')->result_array();
+        $page_data['page_name']  = 'teacher';
+        $page_data['page_title'] = get_phrase('manage_teacher');
+        $this->load->view('backend/index', $page_data);
+    }
+    
+    
+    /***********************************************************************************************************/
+    
+    
+    
+    /****MANAGE SUBJECTS*****/
+    function subject($param1 = '', $param2 = '')
+    {
+        if ($this->session->userdata('personnel_login') != 1)
+            redirect(base_url(), 'refresh');
+        
+        $personnel_profile         = $this->db->get_where('personnel', array(
+            'personnel_id' => $this->session->userdata('personnel_id')
+        ))->row();
+        $personnel_class_id        = $personnel_profile->class_id;
+        $page_data['subjects']   = $this->db->get_where('subject', array(
+            'class_id' => $personnel_class_id
+        ))->result_array();
+        $page_data['page_name']  = 'subject';
+        $page_data['page_title'] = get_phrase('manage_subject');
+        $this->load->view('backend/index', $page_data);
+    }
+    
+    
+    
+    function personnel_marksheet($personnel_id = '') {
+        if ($this->session->userdata('personnel_login') != 1)
+            redirect('login', 'refresh');
+        $class_id     = $this->db->get_where('personnel' , array('personnel_id' => $personnel_id))->row()->class_id;
+        $personnel_name = $this->db->get_where('personnel' , array('personnel_id' => $personnel_id))->row()->name;
+        $class_name   = $this->db->get_where('class' , array('class_id' => $class_id))->row()->name;
+        $page_data['page_name']  =   'personnel_marksheet';
+        $page_data['page_title'] =   get_phrase('marksheet_for') . ' ' . $personnel_name . ' (' . get_phrase('class') . ' ' . $class_name . ')';
+        $page_data['personnel_id'] =   $personnel_id;
+        $page_data['class_id']   =   $class_id;
+        $this->load->view('backend/index', $page_data);
+    }
+
+    function personnel_marksheet_print_view($personnel_id , $exam_id) {
+        if ($this->session->userdata('personnel_login') != 1)
+            redirect('login', 'refresh');
+        $class_id     = $this->db->get_where('personnel' , array('personnel_id' => $personnel_id))->row()->class_id;
+        $class_name   = $this->db->get_where('class' , array('class_id' => $class_id))->row()->name;
+
+        $page_data['personnel_id'] =   $personnel_id;
+        $page_data['class_id']   =   $class_id;
+        $page_data['exam_id']    =   $exam_id;
+        $this->load->view('backend/personnel/personnel_marksheet_print_view', $page_data);
+    }
+    
+    
+    /**********MANAGING CLASS ROUTINE******************/
+    function class_routine($param1 = '', $param2 = '', $param3 = '')
+    {
+        if ($this->session->userdata('personnel_login') != 1)
+            redirect(base_url(), 'refresh');
+        
+        $personnel_profile         = $this->db->get_where('personnel', array(
+            'personnel_id' => $this->session->userdata('personnel_id')
+        ))->row();
+        $page_data['class_id']   = $personnel_profile->class_id;
+        $page_data['page_name']  = 'class_routine';
+        $page_data['page_title'] = get_phrase('manage_class_routine');
+        $this->load->view('backend/index', $page_data);
+    }
+    
+    /******MANAGE BILLING / INVOICES WITH STATUS*****/
+    function invoice($param1 = '', $param2 = '', $param3 = '')
+    {
+        //if($this->session->userdata('personnel_login')!=1)redirect(base_url() , 'refresh');
+        if ($param1 == 'make_payment') {
+            $invoice_id      = $this->input->post('invoice_id');
+            $system_settings = $this->db->get_where('settings', array(
+                'type' => 'paypal_email'
+            ))->row();
+            $invoice_details = $this->db->get_where('invoice', array(
+                'invoice_id' => $invoice_id
+            ))->row();
+            
+            /****TRANSFERRING USER TO PAYPAL TERMINAL****/
+            $this->paypal->add_field('rm', 2);
+            $this->paypal->add_field('no_note', 0);
+            $this->paypal->add_field('item_name', $invoice_details->title);
+            $this->paypal->add_field('amount', $invoice_details->amount);
+            $this->paypal->add_field('custom', $invoice_details->invoice_id);
+            $this->paypal->add_field('business', $system_settings->description);
+            $this->paypal->add_field('notify_url', base_url() . 'index.php?personnel/invoice/paypal_ipn');
+            $this->paypal->add_field('cancel_return', base_url() . 'index.php?personnel/invoice/paypal_cancel');
+            $this->paypal->add_field('return', base_url() . 'index.php?personnel/invoice/paypal_success');
+            
+            $this->paypal->submit_paypal_post();
+            // submit the fields to paypal
+        }
+        if ($param1 == 'paypal_ipn') {
+            if ($this->paypal->validate_ipn() == true) {
+                $ipn_response = '';
+                foreach ($_POST as $key => $value) {
+                    $value = urlencode(stripslashes($value));
+                    $ipn_response .= "\n$key=$value";
+                }
+                $data['payment_details']   = $ipn_response;
+                $data['payment_timestamp'] = strtotime(date("m/d/Y"));
+                $data['payment_method']    = 'paypal';
+                $data['status']            = 'paid';
+                $invoice_id                = $_POST['custom'];
+                $this->db->where('invoice_id', $invoice_id);
+                $this->db->update('invoice', $data);
+
+                $data2['method']       =   'paypal';
+                $data2['invoice_id']   =   $_POST['custom'];
+                $data2['timestamp']    =   strtotime(date("m/d/Y"));
+                $data2['payment_type'] =   'income';
+                $data2['title']        =   $this->db->get_where('invoice' , array('invoice_id' => $data2['invoice_id']))->row()->title;
+                $data2['description']  =   $this->db->get_where('invoice' , array('invoice_id' => $data2['invoice_id']))->row()->description;
+                $data2['personnel_id']   =   $this->db->get_where('invoice' , array('invoice_id' => $data2['invoice_id']))->row()->personnel_id;
+                $data2['amount']       =   $this->db->get_where('invoice' , array('invoice_id' => $data2['invoice_id']))->row()->amount;
+                $this->db->insert('payment' , $data2);
+            }
+        }
+        if ($param1 == 'paypal_cancel') {
+            $this->session->set_flashdata('flash_message', get_phrase('payment_cancelled'));
+            redirect(base_url() . 'index.php?personnel/invoice/', 'refresh');
+        }
+        if ($param1 == 'paypal_success') {
+            $this->session->set_flashdata('flash_message', get_phrase('payment_successfull'));
+            redirect(base_url() . 'index.php?personnel/invoice/', 'refresh');
+        }
+        $personnel_profile         = $this->db->get_where('personnel', array(
+            'personnel_id'   => $this->session->userdata('personnel_id')
+        ))->row();
+        $personnel_id              = $personnel_profile->personnel_id;
+        $page_data['invoices']   = $this->db->get_where('invoice', array(
+            'personnel_id' => $personnel_id
+        ))->result_array();
+        $page_data['page_name']  = 'invoice';
+        $page_data['page_title'] = get_phrase('manage_invoice/payment');
+        $this->load->view('backend/index', $page_data);
+    }
+    
+    /**********MANAGE LIBRARY / BOOKS********************/
+    function book($param1 = '', $param2 = '', $param3 = '')
+    {
+        if ($this->session->userdata('personnel_login') != 1)
+            redirect('login', 'refresh');
+        
+        $page_data['books']      = $this->db->get('book')->result_array();
+        $page_data['page_name']  = 'book';
+        $page_data['page_title'] = get_phrase('manage_library_books');
+        $this->load->view('backend/index', $page_data);
+        
+    }
+    /**********MANAGE TRANSPORT / VEHICLES / ROUTES********************/
+    function transport($param1 = '', $param2 = '', $param3 = '')
+    {
+        if ($this->session->userdata('personnel_login') != 1)
+            redirect('login', 'refresh');
+        
+        $page_data['transports'] = $this->db->get('transport')->result_array();
+        $page_data['page_name']  = 'transport';
+        $page_data['page_title'] = get_phrase('manage_transport');
+        $this->load->view('backend/index', $page_data);
+        
+    }
+    /**********MANAGE DORMITORY / HOSTELS / ROOMS ********************/
+    function dormitory($param1 = '', $param2 = '', $param3 = '')
+    {
+        if ($this->session->userdata('personnel_login') != 1)
+            redirect('login', 'refresh');
+        
+        $page_data['dormitories'] = $this->db->get('dormitory')->result_array();
+        $page_data['page_name']   = 'dormitory';
+        $page_data['page_title']  = get_phrase('manage_dormitory');
+        $this->load->view('backend/index', $page_data);
+        
+    }
+    
+    /**********WATCH NOTICEBOARD AND EVENT ********************/
+    function noticeboard($param1 = '', $param2 = '', $param3 = '')
+    {
+        if ($this->session->userdata('personnel_login') != 1)
+            redirect('login', 'refresh');
+        
+        $page_data['notices']    = $this->db->get('noticeboard')->result_array();
+        $page_data['page_name']  = 'noticeboard';
+        $page_data['page_title'] = get_phrase('noticeboard');
+        $this->load->view('backend/index', $page_data);
+        
+    }
+    
+    /**********MANAGE DOCUMENT / home work FOR A SPECIFIC CLASS or ALL*******************/
+    function document($do = '', $document_id = '')
+    {
+        if ($this->session->userdata('personnel_login') != 1)
+            redirect('login', 'refresh');
+        
+        $page_data['page_name']  = 'manage_document';
+        $page_data['page_title'] = get_phrase('manage_documents');
+        $page_data['documents']  = $this->db->get('document')->result_array();
+        $this->load->view('backend/index', $page_data);
+    }
+    
+    /* private messaging */
+
+    function message($param1 = 'message_home', $param2 = '', $param3 = '') {
+        if ($this->session->userdata('personnel_login') != 1)
+            redirect(base_url(), 'refresh');
+
+        if ($param1 == 'send_new') {
+            $message_thread_code = $this->crud_model->send_new_private_message();
+            $this->session->set_flashdata('flash_message', get_phrase('message_sent!'));
+            redirect(base_url() . 'index.php?personnel/message/message_read/' . $message_thread_code, 'refresh');
+        }
+
+        if ($param1 == 'send_reply') {
+            $this->crud_model->send_reply_message($param2);  //$param2 = message_thread_code
+            $this->session->set_flashdata('flash_message', get_phrase('message_sent!'));
+            redirect(base_url() . 'index.php?personnel/message/message_read/' . $param2, 'refresh');
+        }
+
+        if ($param1 == 'message_read') {
+            $page_data['current_message_thread_code'] = $param2;  // $param2 = message_thread_code
+            $this->crud_model->mark_thread_messages_read($param2);
+        }
+
+        $page_data['message_inner_page_name']   = $param1;
+        $page_data['page_name']                 = 'message';
+        $page_data['page_title']                = get_phrase('private_messaging');
+        $this->load->view('backend/index', $page_data);
+    }
+    
+    /******MANAGE OWN PROFILE AND CHANGE PASSWORD***/
+    function manage_profile($param1 = '', $param2 = '', $param3 = '')
+    {
+        if ($this->session->userdata('personnel_login') != 1)
+            redirect(base_url() . 'index.php?login', 'refresh');
+        if ($param1 == 'update_profile_info') {
+            $data['name']        = $this->input->post('name');
+            $data['email']       = $this->input->post('email');
+            
+            $this->db->where('personnel_id', $this->session->userdata('personnel_id'));
+            $this->db->update('personnel', $data);
+            move_uploaded_file($_FILES['userfile']['tmp_name'], 'uploads/personnel_image/' . $this->session->userdata('personnel_id') . '.jpg');
+            $this->session->set_flashdata('flash_message', get_phrase('account_updated'));
+            redirect(base_url() . 'index.php?personnel/manage_profile/', 'refresh');
+        }
+        if ($param1 == 'change_password') {
+            $data['password']             = $this->input->post('password');
+            $data['new_password']         = $this->input->post('new_password');
+            $data['confirm_new_password'] = $this->input->post('confirm_new_password');
+            
+            $current_password = $this->db->get_where('personnel', array(
+                'personnel_id' => $this->session->userdata('personnel_id')
+            ))->row()->password;
+            if ($current_password == $data['password'] && $data['new_password'] == $data['confirm_new_password']) {
+                $this->db->where('personnel_id', $this->session->userdata('personnel_id'));
+                $this->db->update('personnel', array(
+                    'password' => $data['new_password']
+                ));
+                $this->session->set_flashdata('flash_message', get_phrase('password_updated'));
+            } else {
+                $this->session->set_flashdata('flash_message', get_phrase('password_mismatch'));
+            }
+            redirect(base_url() . 'index.php?personnel/manage_profile/', 'refresh');
+        }
+        $page_data['page_name']  = 'manage_profile';
+        $page_data['page_title'] = get_phrase('manage_profile');
+        $page_data['edit_data']  = $this->db->get_where('personnel', array(
+            'personnel_id' => $this->session->userdata('personnel_id')
+        ))->result_array();
+        $this->load->view('backend/index', $page_data);
+    }
+    
+    /*****************SHOW STUDY MATERIAL / for personnels of a specific class*******************/
+    function study_material($task = "", $document_id = "")
+    {
+        if ($this->session->userdata('personnel_login') != 1)
+        {
+            $this->session->set_userdata('last_page' , current_url());
+            redirect(base_url(), 'refresh');
+        }
+        
+        $data['study_material_info']    = $this->crud_model->select_study_material_info_for_personnel();
+        $data['page_name']              = 'study_material';
+        $data['page_title']             = get_phrase('study_material');
+        $this->load->view('backend/index', $data);
+    }
+}
